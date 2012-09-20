6d4500c2803ed027a951299c363aa4798fe09a72
Chris Withers <chris@simplistix.co.uk>
2012-09-20 07:33:41 +0100

    Support for constants.

--- fixed/__init__.py
+++ fixed/__init__.py
@@ -4,6 +4,8 @@
 from collections import namedtuple
 from operator import attrgetter
 
+import re
+
 # field types
 
 class Ordered(object):
@@ -19,6 +21,16 @@ class Field(Ordered):
         self.size = size
         self.convertor = convertor
         self.start = start
+        if isinstance(convertor, one_of):
+            for attr, const in convertor.attrs.items():
+                if attr in self.__dict__:
+                    raise AttributeError('Constant cannot be stored as %r' % attr)
+                if len(const.text) != self.size:
+                    raise TypeError('%r does not have a size of %i' % (
+                        const, self.size
+                        ))
+                setattr(self, attr, const)
+                
 
 class Discriminator(Field):
     def __init__(self, text, *args, **kw):
@@ -28,4 +40,37 @@ class Discriminator(Field):
 class Skip(Ordered):
     def __init__(self, size):
         self.size = size
+
+class Constant(object):
+    def __init__(self, text, description=None):
+        self.text = text
+        self.description = description or text
+    def __repr__(self):
+        return '<%s>' % self.description
+    def __str__(self):
+        return self.text
+
+_attr_re = re.compile('\W+')
+
+class one_of(dict):
+    
+    def __init__(self, *consts, **kw):
+        self.attrs = {}
+        for const in consts:
+            if not isinstance(const, Constant):
+                raise TypeError('Not a constant: %r' % const)
+            self[const.text] = const
+            attr = _attr_re.sub('_', const.description).strip('_')
+            self.attrs[attr] = const
+        for attr, const in kw.items():
+            self[const.text] = consts
+            self.attrs[attr] = const
+
+    def __setitem__(self, text, value):
+        if text in self:
+            raise TypeError('Duplicate constant: %r' % text)
+        super(one_of, self).__setitem__(text, value)
+        
+    def __call__(self, text):
+        return self[text]
 
--- fixed/tests/test_fields.py
+++ fixed/tests/test_fields.py
@@ -3,9 +3,9 @@
 
 from unittest import TestCase
 
-from testfixtures import compare
+from testfixtures import ShouldRaise, compare
 
-from .. import Discriminator, Field, Skip
+from .. import Discriminator, Field, Skip, Constant, one_of
 
 class TestField(TestCase):
 
@@ -28,6 +28,61 @@ class TestField(TestCase):
         f1 = Field(1)
         f2 = Field(2)
         self.assertTrue(f1.order < f2.order)
+
+class TestConstants(TestCase):
+
+    def test_attributes(self):
+        c = Constant('X', "Execution")
+        compare(c.text, 'X')
+        compare(c.description, "Execution")
+
+    def test_repr_str(self):
+        c = Constant('X', "Execution")
+        compare(repr(c), '<Execution>')
+        compare(str(c), 'X')
+    
+    def test_default_description(self):
+        c = Constant('X')
+        compare(c.text, 'X')
+        compare(c.description, "X")
+        compare(repr(c), '<X>')
+        compare(str(c), 'X')
+
+    def test_one_of(self):
+        x = Constant('X')
+        y = Constant('Y')
+        convertor = one_of(x, y)
+        self.assertTrue(convertor('X') is x)
+        self.assertTrue(convertor('Y') is y)
+
+    def test_one_of_duplicate_constant(self):
+        with ShouldRaise(TypeError("Duplicate constant: 'X'")):
+            one_of(Constant('X'), Constant('X'))
+
+    def test_one_of_not_a_constant(self):
+        with ShouldRaise(TypeError("Not a constant: 'X'")):
+            one_of('X')
+    
+    def test_field(self):
+        cx = Constant('X', 'foo')
+        cy = Constant('Y', 'bar & baz (&_)')
+        cz = Constant('Z')
+        f = Field(1, one_of(cx, cy, z = cz))
+        self.assertTrue(f.foo is cx)
+        self.assertTrue(f.bar_baz is cy)
+        self.assertTrue(f.z is cz)
+
+    def test_field_overwrite_attr(self):
+        with ShouldRaise(AttributeError("Constant cannot be stored as 'size'")):
+            Field(1, one_of(Constant('S', 'size')))
+        
+    def test_field_overwrite_const_attr(self):
+        with ShouldRaise(AttributeError("Constant cannot be stored as 'size'")):
+            Field(1, one_of(Constant('S', 'size'), Constant('T', 'size')))
+        
+    def test_field_different_widths(self):
+        with ShouldRaise(TypeError("<XX> does not have a size of 1")):
+            Field(1, one_of(Constant('XX')))
 
 class TestDiscriminator(TestCase):
 
